a4557146d133b3f179f065e7af045d03a76031ad
Yuvarajan V K <innovatoryuvarajanvk@gmail.com>
2024-10-29 10:00:02 +0530

    Added Booking and Event Models

---
+++ TicketReservationAPI/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketReservationAPI.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+
+        public string UserName { get; set; }
+        public int NumberOfTickets { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string BookingReference { get; set; }
+
+    }
+}
---
+++ TicketReservationAPI/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace TicketReservationAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Venue { get; set; }
+        public int TotalSeats { get; set; }
+        public int AvailableSeats { get; set; }
+    }
+}
